9225e2dc66d3779608e90bcfe17ed0a1ebec6f36
Emin Kodaz <eminkodazz@gmail.com>
2023-06-14 21:36:18 +0300

    added some anims and weapon switching mechanics

--- Secret of the Future U-15/Assets/Scripts/WeaponManager.cs
+++ Secret of the Future U-15/Assets/Scripts/WeaponManager.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class WeaponManager : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
+    public GameObject pistol;
+    public GameObject rifle;
+
+
     float nextTime = 0;
     bool isReloading = false;
 
@@ -19,24 +23,6 @@ public class WeaponManager : MonoBehaviour
 
     private void Update()
     {
-        nextTime += Time.time;
-        if (nextTime >= 2f && !isReloading)
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                pistolShootSound.Play();
-                _animator.SetTrigger("Fire");
-                nextTime = 0;
-            }
-        }
-
-        /*if (Input.GetMouseButtonDown(0)) 
-        {
-            pistolShootSound.Play();
-            _animator.SetBool("isShooting", true);
-            StartCoroutine(ShootDelay(0.1f));       
-        }*/
-
         if (Input.GetKeyDown(KeyCode.R) && !isReloading) 
         {
             isReloading = true;
@@ -59,14 +45,67 @@ public class WeaponManager : MonoBehaviour
             _animator.SetBool("isMove", false);
         }
 
+        if (pistol != null && pistol.activeSelf)
+        {
+            nextTime += Time.time;
+            if (nextTime >= 2f && !isReloading)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    pistolShootSound.Play();
+                    _animator.SetTrigger("Fire");
+                    nextTime = 0;
+                }
+            }
+        }
+
+        if (pistol != null && rifle.activeSelf)
+        {
+            nextTime += Time.time;
+            if (nextTime >= 2f && !isReloading)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    _animator.SetTrigger("Fire");
+                    nextTime = 0;
+                }
+            }
+        }
+
+
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if (pistol != null)
+            {
+                pistol.SetActive(true);
+                rifle.SetActive(false);
+
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (rifle != null)
+            {
+                pistol.SetActive(false);
+                rifle.SetActive(true);
+
+
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            
+        }
+
 
     }
 
-    IEnumerator ShootDelay(float delay)
-    {
-        yield return new WaitForSeconds(delay);
-        _animator.SetBool("isShooting", false);
-    }
+    //IEnumerator ShootDelay(float delay)
+    //{
+    //    yield return new WaitForSeconds(delay);
+    //    _animator.SetBool("isShooting", false);
+    //}
 
     IEnumerator ReloadDone(float delay)
     {
@@ -75,9 +114,9 @@ public class WeaponManager : MonoBehaviour
         _animator.SetBool("reload", isReloading);
     }
 
-    IEnumerator RunChecker(float delay)
-    {
-        yield return new WaitForSeconds(delay);
-        _animator.SetBool("isMove", false);
-    }
+    //IEnumerator RunChecker(float delay)
+    //{
+    //    yield return new WaitForSeconds(delay);
+    //    _animator.SetBool("isMove", false);
+    //}
 }
