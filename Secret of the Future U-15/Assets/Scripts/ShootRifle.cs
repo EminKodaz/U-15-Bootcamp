0cd75213222f3614c411967fc06c398cfbf3798f
Yusuf <yusuf_zeki1234@hotmail.com>
2023-06-20 11:29:15 +0300

    Rifle added to scop

--- Secret of the Future U-15/Assets/Scripts/ShootRifle.cs
+++ Secret of the Future U-15/Assets/Scripts/ShootRifle.cs
@@ -10,7 +10,6 @@ public class ShootRifle : WeaponManager
     {
         focusGuns = !focusGuns;
         camAnim.SetBool("focus", focusGuns);
-        StartCoroutine(ScopeOverlayZoomDelay());
     }
 
     public override void Shoots()
@@ -20,32 +19,5 @@ public class ShootRifle : WeaponManager
         shootManager.Shoot();
         _animator.SetTrigger("Fire");
         nextTime = 0;
-    }
-
-    IEnumerator ScopeOverlayZoomDelay()
-    {
-        yield return new WaitForSeconds(0.3f);
-
-        if (focusGuns)
-        {
-            fpsCam.cullingMask = fpsCam.cullingMask & ~(1 << 11);
-            fpsCam.fieldOfView = 20;
-            GetComponentInParent<FirstPersonController>().MoveSpeed = 2;
-        }
-        else
-        {
-            fpsCam.cullingMask = fpsCam.cullingMask | (1 << 11);
-            fpsCam.fieldOfView = 75;
-            GetComponentInParent<FirstPersonController>().MoveSpeed = 4;
-        }
-        scopeOverlay.SetActive(focusGuns);
-    }
-    private void OnDisable()
-    {
-        fpsCam.cullingMask = fpsCam.cullingMask | (1 << 11);
-        fpsCam.fieldOfView = 75;
-        GetComponentInParent<FirstPersonController>().MoveSpeed = 4;
-        scopeOverlay.SetActive(false);
-
     }
 }
