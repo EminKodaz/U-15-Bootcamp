1897c5e14b32245471d99bf2a98b1a98f94231c0
Emin Kodaz <eminkodazz@gmail.com>
2023-06-20 22:48:04 +0300

    npc added

---
+++ Secret of the Future U-15/Assets/Scripts/ZombieAI.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class ZombieAI : MonoBehaviour
+{
+
+    public float zombieHealth;
+    public GameObject target;
+    public Transform player;
+    public bool triggered = false;
+    NavMeshAgent agent;
+    Animator animator;
+
+
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (triggered)
+        {
+            transform.LookAt(target.gameObject.transform);
+            agent.destination = player.position;
+        }
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            animator.SetBool("Scream", true);
+            animator.SetBool("Run", true);
+            Debug.Log("Sa");
+
+
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            triggered = true;
+            animator.SetBool("Attack", false);
+            animator.SetBool("Scream", false);
+        }
+    }
+
+}
