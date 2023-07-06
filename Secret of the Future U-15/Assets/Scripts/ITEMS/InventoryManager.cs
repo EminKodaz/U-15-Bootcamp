63f82bea8ac7a9531d0724eb3b7147d1abf3163a
Yusuf <yusuf_zeki1234@hotmail.com>
2023-07-06 16:23:30 +0300

    Health item added

--- Secret of the Future U-15/Assets/Scripts/ITEMS/InventoryManager.cs
+++ Secret of the Future U-15/Assets/Scripts/ITEMS/InventoryManager.cs
@@ -8,7 +8,8 @@ public class InventoryManager : MonoBehaviour
     public static InventoryManager Instance;
     public List<Item> ›tems = new List<Item>();
 
-    public Transform itemContent;
+    public Transform itemContentBullet;
+    public Transform itemContentHealth;
     public GameObject Inventory›tem;
     public int Total›temCount = 4;
     public int Current›tem = 0;
@@ -30,28 +31,49 @@ public class InventoryManager : MonoBehaviour
 
     public void Remove(Item item)
     {
+        Current›tem--;
         ›tems.Remove(item);
     }
 
     public void List›tems()
     {
-        foreach (Transform item in itemContent)
+        foreach (Transform item in itemContentBullet)
+        {
+            Destroy(item.gameObject);
+        }
+        foreach (Transform item in itemContentHealth)
         {
             Destroy(item.gameObject);
         }
 
         foreach (var item in ›tems)
         {
-            GameObject obj = Instantiate(Inventory›tem, itemContent);
-            //var itemName = obj.transform.Find("Item/itemName").GetComponent<Text>();
-            var itemIcon = obj.transform.Find("icon").GetComponent<Image>();
-            var itemName = obj.transform.Find("BulletType").GetComponent<Text>();
-            obj.transform.GetComponent<Button›tem>().id = item.id;
-            obj.transform.GetComponent<Button›tem>().item = item;
+            if (item.HealthOrBullet == true)
+            {
+                GameObject obj = Instantiate(Inventory›tem, itemContentHealth);
+                //var itemName = obj.transform.Find("Item/itemName").GetComponent<Text>();
+                var itemIcon = obj.transform.Find("icon").GetComponent<Image>();
+                var itemName = obj.transform.Find("BulletType").GetComponent<Text>();
+                obj.transform.GetComponent<Button›tem>().id = item.id;
+                obj.transform.GetComponent<Button›tem>().item = item;
 
-            //itemName.text = item.itemName;
-            itemIcon.sprite = item.icon;
-            itemName.text = item.itemName;
+                //itemName.text = item.itemName;
+                itemIcon.sprite = item.icon;
+                itemName.text = item.itemName;
+            }
+            else
+            {
+                GameObject obj = Instantiate(Inventory›tem, itemContentBullet);
+                //var itemName = obj.transform.Find("Item/itemName").GetComponent<Text>();
+                var itemIcon = obj.transform.Find("icon").GetComponent<Image>();
+                var itemName = obj.transform.Find("BulletType").GetComponent<Text>();
+                obj.transform.GetComponent<Button›tem>().id = item.id;
+                obj.transform.GetComponent<Button›tem>().item = item;
+
+                //itemName.text = item.itemName;
+                itemIcon.sprite = item.icon;
+                itemName.text = item.itemName;
+            }
         }
     }
 }
