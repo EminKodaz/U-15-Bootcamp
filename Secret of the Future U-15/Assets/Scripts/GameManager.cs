e02f508f6d997b52392f80f91ad159b32adabe92
Yusuf <yusuf_zeki1234@hotmail.com>
2023-07-14 11:49:58 +0300

    Last scene load added and last scene charecter updated

--- Secret of the Future U-15/Assets/Scripts/GameManager.cs
+++ Secret of the Future U-15/Assets/Scripts/GameManager.cs
@@ -8,6 +8,8 @@ public class GameManager : MonoBehaviour
 {
     public GameObject PauseObject;
 
+    [SerializeField] private CharecterController m_Charecter;
+
     public bool open;
     public bool isFollowingDad = false;
 
@@ -16,6 +18,10 @@ public class GameManager : MonoBehaviour
     public GameObject HurtImage;
     [SerializeField] private Text KillCount;
     private int Kill;
+
+    [Header("Load Options")]
+    public GameObject LoadScene;
+    public Image LoadingFillImage;
 
     private void Awake()
     {
@@ -40,17 +46,32 @@ public class GameManager : MonoBehaviour
     {
         if (open)
         {
+            if (m_Charecter != null)
+            {
+                m_Charecter.speed = 0;
+            }
             Time.timeScale = 0;
             PauseObject.SetActive(true);
             Cursor.lockState = CursorLockMode.Confined;
-            HurtImage.SetActive(false);
+            if (HurtImage != null)
+            {
+                HurtImage.SetActive(false);
+            }
         }
         else
         {
+            if (m_Charecter != null)
+            {
+                m_Charecter.speed = 5f;
+            }
+
             Time.timeScale = 1;
             PauseObject.SetActive(false);
             Cursor.lockState = CursorLockMode.Locked;
-            HurtImage.SetActive(true);
+            if (HurtImage != null)
+            {
+                HurtImage.SetActive(true);
+            }
         }
     }
 
@@ -70,5 +91,28 @@ public class GameManager : MonoBehaviour
     public void KillCalculate(int _kill)
     {
         Kill += _kill;
+    }
+
+    public void LoadScenes(int Scene›d)
+    {
+        StartCoroutine(LoadSceneAsync(Scene›d));
+    }
+
+    IEnumerator LoadSceneAsync(int Scene›d)
+    {
+        LoadScene.SetActive(true);
+
+        yield return new WaitForEndOfFrame();
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(Scene›d);
+
+        while (!operation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(operation.progress / 0.9f);
+
+            LoadingFillImage.fillAmount = progressValue;
+
+            yield return null;
+        }
     }
 }
